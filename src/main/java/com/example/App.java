94b7c7a6d4432dd541ea7277af5e2cfe694a967a
Mataiaz <mathias344@live.no>
2022-02-09 23:36:37 +0100

    fixed condition to run pre set arguments in case of wrong input from user

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,22 +8,27 @@ public final class App {
     public static void main(String[] args) throws IOException {
         
         //If theres no arguments from main
-        if (args[0] == "")
-        {
-            String url = "https://9anime.to/newest?page="; //Link to site you want to fetch data from
-            int pages = 3; //Select amount of pages it crawls through
-            int posts = 10; //Select amount of posts it fetches from page(2 pages is double the posts)
-            Crawler.main(url, pages, posts); //Algorithm which takes in the url, pages and posts given above
-        }
-        else
-        {
+        try {
             String url = args[0]; //Link to site you want to fetch data from
             int pages = Integer.parseInt(args[1]); //Select amount of pages it crawls through
             int posts = Integer.parseInt(args[2]); //Select amount of posts it fetches from page(2 pages is double the posts)
             
             Crawler.main(url, pages, posts); //Algorithm which takes in the url, pages and posts given above
+        } catch (Exception e) {
+            //Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 0 out of bounds for length 0
+            String eString = e.toString();
+            if (eString.equals("java.lang.ArrayIndexOutOfBoundsException: Index 0 out of bounds for length 0"))
+                {
+                    System.out.println("RUNNING WITH PREMADE INPUT");
+                    String url = "https://9anime.to/newest?page="; //Link to site you want to fetch data from
+                    int pages = 3; //Select amount of pages it crawls throughw
+                    int posts = 10; //Select amount of posts it fetches from page(2 pages is double the posts)
+                    Crawler.main(url, pages, posts); //Algorithm which takes in the url, pages and posts given above
+                }
+                else
+                {
+                    System.out.println("Program Faild to run " + e);
+                }
         }
-        
     }
-
 }
